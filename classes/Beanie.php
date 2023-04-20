a0106e4e92a5f1f8ee87352720c05c885682a1f7
HueKhuc <khuchue.hus@gmail.com>
2023-04-20 15:47:13 +0200

    23. Liste et filtres + 24. Retenir les valeurs entrées dans notre formulaire

--- classes/Beanie.php
+++ classes/Beanie.php
@@ -1,20 +1,87 @@
 <?php
 class Beanie
-{
-    public function getName(): ?string
+{    
+    const AVAILABLE_SIZES = ["S", "M", "L", "XL"];
+
+    const MATERIAL_WOOL = 'wool';
+    const MATERIAL_SILK = 'silk';
+    const MATERIAL_COTTON = 'cotton';
+    const MATERIAL_CASHMERE = 'cashmere';
+
+    const AVAILABLE_MATERIALS = [
+        self::MATERIAL_WOOL => 'laine',
+        self::MATERIAL_SILK => 'soie',
+        self::MATERIAL_COTTON => 'coton',
+        self::MATERIAL_CASHMERE => 'cachemire'
+    ];
+    protected ?string $material;
+    protected array $materials = [];
+    protected array $sizes = [];
+    protected ?string $size;
+    protected ?string $name;
+    protected float $price = 0.0;
+    protected ?string $image;
+    protected ?string $description;
+
+    
+    public function getSizes():array
+    {
+        return $this->sizes;
+    }
+    public function setSizes(array $sizes = []):self
+    {
+        foreach ($sizes as $size) {
+            $this->addSize($size);
+        }
+        return $this;
+    }
+    public function addSize(string $size):self
+    {
+        if (!in_array($size,self::AVAILABLE_SIZES)) {
+            return $this;
+        }
+        if (!in_array($size, $this->sizes)) {
+            $this->sizes[$size] = $size;
+        }
+        return $this;
+    }
+    public function getMaterial(): array
+    {
+        return $this->materials;
+    }
+    public function setMaterials(array $materials = []): self
+    {
+        foreach ($materials as $material) {
+            $this->addMaterial($material);
+        }
+        return $this;
+    }
+    public function addMaterial(string $material): self
+    {
+        if (!isset(self::AVAILABLE_MATERIALS[$material])) {
+            return $this;
+        }
+        if (!in_array($material, $this->materials)) {
+            $this->materials[$material] = $material;
+        }
+
+        return $this;
+    }
+
+    public function getName(): string
     {
         return ucfirst(strtolower($this->name));
     }
-    public function setName(?string $name): Beanie
+    public function setName( $name): self
     {
         $this->name = $name;
         return $this;
     }
-    public function getPrice(): ?float
+    public function getPrice()
     {
         return ($this->price);
     }
-    public function setPrice(?float $price): Beanie
+    public function setPrice( $price): self
     {
         $this->price = $price;
         return $this;
@@ -23,7 +90,7 @@ class Beanie
     {
         return ucfirst(strtolower($this->description));
     }
-    public function setDescription(?string $description): Beanie
+    public function setDescription( $description): self
     {
         $this->description = $description;
         return $this;
@@ -32,7 +99,7 @@ class Beanie
     {
         return $this->image;
     }
-    public function setImage(?string $image): Beanie
+    public function setImage($image): self
     {
         $this->image = $image;
         return $this;
--- pages/list.php
+++ pages/list.php
@@ -1,12 +1,96 @@
+<?php
+$beaniesFiltered = $mesProduits;
+$material = null;
+$minPrice = null;
+$maxPrice = null;
+$size = null;
+
+
+if (!empty($_POST['material'])) {
+    $material = trim($_POST['material']);
+
+    $beaniesFiltered = array_filter($beaniesFiltered, function (Beanie $beanie) use ($material) {
+        return in_array($material, $beanie->getMaterial());
+    });
+}
+if (!empty($_POST['size'])) {
+    $size = trim($_POST['size']);
+
+    $beaniesFiltered = array_filter($beaniesFiltered, function (Beanie $beanie) use ($size) {
+        return in_array($size, $beanie->getSizes());
+    });
+}
+if (!empty($_POST['minPrice'])) {
+    $minPrice = floatval($_POST['minPrice']);
+
+    $beaniesFiltered = array_filter($beaniesFiltered, function (Beanie $beanie) use ($minPrice) {
+        return $beanie->getPrice() >= $minPrice;
+    });
+}
+if (!empty($_POST['maxPrice'])) {
+    $maxPrice = floatval($_POST['maxPrice']);
+
+    $beaniesFiltered = array_filter($beaniesFiltered, function (Beanie $beanie) use ($maxPrice) {
+        return $beanie->getPrice() <= $maxPrice;
+    });
+}
+?>
+<form action="" method="POST">
+    <div class="form-row d-flex flex-row gap-2 m-3">
+        <div class="form-group d-flex flex-row gap-2 align-items-center col-md-2">
+            <label for="material" class="mr-sm-2">Matériel</label>
+            <select name="material" class="form-control custom-select mr-sm-2">
+                <option value="">Choose...</option>
+
+                <?php
+                foreach (Beanie::AVAILABLE_MATERIALS as $value => $name) {
+                    echo '<option value="' . $value . '"';
+                    if ($value == $material) {
+                        echo 'selected';
+                    }
+                    echo '>' . $name . '</option>';
+                }
+                ?>
+            </select>
+        </div>
+
+        <div class="form-group d-flex flex-row gap-2 align-items-center">
+            <label for="size" class="mr-sm-2">Taille</label>
+            <select name="size" class="form-control custom-select mr-sm-2">
+                <option value="">Choose...</option>
+                <?php
+                foreach (Beanie::AVAILABLE_SIZES as $taille) {
+                    echo '<option value="' . $taille. '"';
+                    if ($taille == $size) {
+                        echo 'selected';
+                    }
+                    echo '>' . $taille . '</option>';
+                }
+                ?>
+            </select>
+        </div>
+
+        <div class="form-group d-flex flex-row gap-2 align-items-center text-center">
+            <label for="minPrice">Min (€)</label>
+            <input type="number" name="minPrice" id="minPrice" class="form-control custom-select"
+                value="<?php echo $minPrice; ?>">
+        </div>
+        <div class="form-group d-flex flex-row gap-2 align-items-center text-center">
+            <label for="maxPrice">Max (€)</label>
+            <input type="number" name="maxPrice" id="maxPrice" class="form-control custom-select"
+                value="<?php echo $maxPrice; ?>">
+        </div>
+        <button class="btn btn-outline-dark align-self-center" type="submit">Filtre</button>
+    </div>
+</form>
+
 <div class="d-flex flex-row justify-content-center gap-5">
     <?php
-    foreach ($mesProduits as $key => $beanie) {
+    foreach ($beaniesFiltered as $key => $beanie) {
         affichageProduit($beanie);
         echo $key . '">
-                Ajouter au panier
-    </a>
-</div>
-</div>';
+                Ajouter au panier </a></div></div>';
     }
     ?>
+
 </div>
