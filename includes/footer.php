302709b8cb68a954ed552a450905e727b469bbd7
HueKhuc <khuchue.hus@gmail.com>
2023-04-27 12:07:02 +0200

    26. Créer une base de données

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,15 @@
 <?php
-$erreurs=[];
+$erreurs = [];
 $contact = new Contact();
 if (!empty($_POST)) {
   $contact = new Contact($_POST);
-  $erreurs=$contact ->validation();
-  if(empty($erreurs)){
-echo '<div class="alert alert-success my-5" role="alert">Message envoyé</div>';
+  $erreurs = $contact->validation();
+  if (empty($erreurs)) {
+    echo '<div class="alert alert-success my-5" role="alert">Message envoyé</div>';
+
+    $sql = 'INSERT INTO contact(sujet,email,message)
+            VALUES ("' . $contact->getSujet() . '","' . $contact->getEmail() . '","' . $contact->getMessage() . '")';
+    $statement = $connection->exec($sql);
   }
 }
 ;
@@ -17,25 +21,25 @@ echo '<div class="alert alert-success my-5" role="alert">Message envoyé</div>';
     <div class="form-floating mb-3">
       <input type="text" class="form-control" placeholder="Sujet" id="floatingTextarea" name="sujet"
         value="<?php echo $contact->getSujet(); ?>">
-        <?php if (!empty($erreurs['sujet'])){
-          echo '<p class="badge text-bg-danger">Entrez le sujet de votre message<p>';
-          }?>
+      <?php if (!empty($erreurs['sujet'])) {
+        echo '<p class="badge text-bg-danger">Entrez le sujet de votre message<p>';
+      } ?>
       <label for="floatingTextarea">Sujet</label>
     </div>
     <div class="form-floating mb-3">
       <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com"
         value="<?php echo $contact->getEmail(); ?>">
-        <?php if (!empty($erreurs['email'])){
-          echo '<p class="badge text-bg-danger">Entrez votre email<p>';
-          }?>
+      <?php if (!empty($erreurs['email'])) {
+        echo '<p class="badge text-bg-danger">Entrez votre email<p>';
+      } ?>
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating mb-3">
       <textarea class="form-control" placeholder="Votre message" id="floatingTextarea2" name="message"
-        style="height: 100px" value="<?php echo $contact->getMessage(); ?>" ></textarea>
-        <?php if (!empty($erreurs['message'])){
-          echo '<p class="badge text-bg-danger">Entrez votre message<p>';
-          }?>
+        style="height: 100px" value="<?php echo $contact->getMessage(); ?>"></textarea>
+      <?php if (!empty($erreurs['message'])) {
+        echo '<p class="badge text-bg-danger">Entrez votre message<p>';
+      } ?>
       <label for="floatingTextarea2">Message</label>
     </div>
     <input type="submit" name="submit" value="Envoyer" class="btn btn-outline-dark">
